ee99383e6e828327e1afd610c3dfeea46731c8ce
satoshi-sh <satoss1108@gmail.com>
2023-01-07 15:44:17 -0600

    change allowed_hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["satoshi40.pythonanywhere.com/"]
+ALLOWED_HOSTS = ["satoshi40.pythonanywhere.com"]
 
 
 # Application definition
